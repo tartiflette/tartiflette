1b3cf44dcdb0498739fbb55415c90740838aa113
Maximilien Raulic <raulic.maximilien@gmail.com>
2019-01-15 13:43:59 +0100

    test(unit): add regression tests on issue 82

---
+++ tests/functional/regressions/test_issue82.py
@@ -0,0 +1,50 @@
+import pytest
+
+from tartiflette.engine import Engine
+from tartiflette.resolver import Resolver
+
+
+@Resolver("Query.viewer", schema_name="test_issue82")
+async def resolver_query_viewer(*_, **__):
+    return {"name": "N1"}
+
+
+_TTFTT_ENGINE = Engine(
+    """
+    type User {
+        name: String
+    }
+    
+    type Query {
+        viewer: User
+    }
+    """,
+    schema_name="test_issue82",
+)
+
+
+@pytest.mark.asyncio
+async def test_issue82():
+    query = """
+    query {
+        viewer {
+            name
+            ...UndefinedFragment
+        }
+    }
+    """
+
+    results = await _TTFTT_ENGINE.execute(query)
+
+    assert results == {
+        "data": None,
+        "errors": [
+            {
+                "message": "Undefined fragment < UndefinedFragment >.",
+                "path": None,
+                "locations": [
+                    {"line": 5, "column": 13}
+                ]
+            }
+        ]
+    }
