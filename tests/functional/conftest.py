7a7ffa628f14d744e38f02e4762b4bb22fbb01dc
Maximilien Raulic <raulic.maximilien@gmail.com>
2019-01-17 12:25:39 +0100

    feat(test-functional): handle a "ttftt_engine" mark to automatically inject engine with parametrize feature

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,6 +1,21 @@
+import os
+
 import pytest
 
+from tartiflette import Engine
 from tartiflette.schema.registry import SchemaRegistry
+
+
+_CURR_PATH = os.path.dirname(os.path.abspath(__file__))
+
+_DEFAULT_ENGINE = Engine(
+    os.path.join(_CURR_PATH, "data", "sdls", "animals.sdl")
+)
+
+_TTFTT_ENGINES = {
+    "default": _DEFAULT_ENGINE,
+    "animals": _DEFAULT_ENGINE,
+}
 
 
 @pytest.yield_fixture
@@ -8,3 +23,20 @@ def clean_registry():
     SchemaRegistry._schemas = {}
     yield SchemaRegistry
     SchemaRegistry._schemas = {}
+
+
+def pytest_generate_tests(metafunc):
+    try:
+        marker = metafunc.definition.get_closest_marker("ttftt_engine")
+        if not marker:
+            return None
+    except AttributeError:
+        return None
+
+    engine_name = marker.kwargs.get("name") or "default"
+    try:
+        engine = _TTFTT_ENGINES[engine_name]
+    except KeyError:
+        engine = _TTFTT_ENGINES["default"]
+
+    metafunc.parametrize("engine", [engine])
