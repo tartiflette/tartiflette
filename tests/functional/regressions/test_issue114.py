c67df3c6b0acfa0150ef27a9f13120a61936871c
Maximilien Raulic <raulic.maximilien@gmail.com>
2019-02-05 18:09:08 +0100

    test(unit): add regression tests on issue 114

---
+++ tests/functional/regressions/test_issue114.py
@@ -0,0 +1,175 @@
+import pytest
+
+
+async def _query_dog_resolver(*_args, **__kwargs):
+    return {"name": "Doggo"}
+
+
+async def _query_human_resolver(*_args, **_kwargs):
+    return {"name": "Hooman"}
+
+
+@pytest.mark.asyncio
+@pytest.mark.ttftt_engine(
+    resolvers={
+        "Query.dog": _query_dog_resolver,
+        "Query.human": _query_human_resolver,
+    }
+)
+@pytest.mark.parametrize(
+    "query,operation_name,expected",
+    [
+        (
+            """
+            query {
+              dog {
+                name
+              }
+            }
+            """,
+            None,
+            {"data": {"dog": {"name": "Doggo"}}},
+        ),
+        (
+            """
+            query Dog {
+              dog {
+                name
+              }
+            }
+            """,
+            "Dog",
+            {"data": {"dog": {"name": "Doggo"}}},
+        ),
+        (
+            """
+            query {
+              dog {
+                name
+              }
+            }
+            """,
+            "Dog",
+            {
+                "data": None,
+                "errors": [
+                    {
+                        "message": "Unknown < Dog > operation.",
+                        "path": None,
+                        "locations": [],
+                    }
+                ],
+            },
+        ),
+        (
+            """
+            query Dog {
+              dog {
+                name
+              }
+            }
+            """,
+            None,
+            {
+                "data": None,
+                "errors": [
+                    {
+                        "message": "No anonymous operation found.",
+                        "path": None,
+                        "locations": [],
+                    }
+                ],
+            },
+        ),
+        (
+            """
+            query Dog {
+              dog {
+                name
+              }
+            }
+            
+            query Human {
+              human(id: 1) {
+                name
+              }
+            }
+            """,
+            None,
+            {
+                "data": None,
+                "errors": [
+                    {
+                        "message": "No anonymous operation found.",
+                        "path": None,
+                        "locations": [],
+                    }
+                ],
+            },
+        ),
+        (
+            """
+            query Dog {
+              dog {
+                name
+              }
+            }
+            
+            query Human {
+              human(id: 1) {
+                name
+              }
+            }
+            """,
+            "Dog",
+            {"data": {"dog": {"name": "Doggo"}}},
+        ),
+        (
+            """
+            query Dog {
+              dog {
+                name
+              }
+            }
+            
+            query Human {
+              human(id: 1) {
+                name
+              }
+            }
+            """,
+            "Human",
+            {"data": {"human": {"name": "Hooman"}}},
+        ),
+        (
+            """
+            query Dog {
+              dog {
+                name
+              }
+            }
+            
+            query Human {
+              human(id: 1) {
+                name
+              }
+            }
+            """,
+            "Unknown",
+            {
+                "data": None,
+                "errors": [
+                    {
+                        "message": "Unknown < Unknown > operation.",
+                        "path": None,
+                        "locations": [],
+                    }
+                ],
+            },
+        ),
+    ],
+)
+async def test_issue114(engine, query, operation_name, expected):
+    assert (
+        await engine.execute(query, operation_name=operation_name) == expected
+    )
