61622cccc6e8d2e740a5f19d579d9cf792bddac5
Maximilien Raulic <raulic.maximilien@gmail.com>
2019-04-18 17:20:53 +0200

    test(unit): add regression tests on issue 213

---
+++ tests/functional/regressions/test_null_nullable.py
@@ -0,0 +1,185 @@
+import pytest
+
+from tartiflette import Engine, Resolver
+
+
+@Resolver("Query.hello", schema_name="test_null_nullable")
+async def resolve_query_hello(parent, args, ctx, info):
+    return args.get("name")
+
+
+@Resolver("Query.bye", schema_name="test_null_nullable")
+async def resolve_query_bye(parent, args, ctx, info):
+    return args.get("name")
+
+
+_SDL = """
+type Query {
+  hello(name: String = "Unknown"): String
+  bye(name: String! = "Unknown"): String
+}
+"""
+
+
+_TTFTT_ENGINE = Engine(_SDL, schema_name="test_null_nullable")
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "query,variables,expected",
+    [
+        # Without variables
+        (
+            """
+            query {
+              hello
+            }
+            """,
+            None,
+            {"data": {"hello": "Unknown"}},
+        ),
+        (
+            """
+            query {
+              hello(name: "Name")
+            }
+            """,
+            None,
+            {"data": {"hello": "Name"}},
+        ),
+        (
+            """
+            query {
+              hello(name: null)
+            }
+            """,
+            None,
+            {"data": {"hello": None}},
+        ),
+        (
+            """
+            query {
+              bye
+            }
+            """,
+            None,
+            {"data": {"bye": "Unknown"}},
+        ),
+        (
+            """
+            query {
+              bye(name: "Name")
+            }
+            """,
+            None,
+            {"data": {"bye": "Name"}},
+        ),
+        (
+            """
+            query {
+              bye(name: null)
+            }
+            """,
+            None,
+            {
+                "data": {"bye": None},
+                "errors": [
+                    {
+                        "message": "Invalid value ("
+                        "value: None) for field `bye` of type `String`",
+                        "locations": [{"column": 15, "line": 3}],
+                        "path": ["bye"],
+                    }
+                ],
+            },
+        ),
+        # With variables
+        (
+            """
+            query ($name: String) {
+              hello(name: $name)
+            }
+            """,
+            {},
+            {"data": {"hello": "Unknown"}},
+        ),
+        (
+            """
+            query ($name: String) {
+              hello(name: $name)
+            }
+            """,
+            {"name": "Name"},
+            {"data": {"hello": "Name"}},
+        ),
+        (
+            """
+            query ($name: String) {
+              hello(name: $name)
+            }
+            """,
+            {"name": None},
+            {"data": {"hello": None}},
+        ),
+        (
+            """
+            query ($name: String) {
+              bye(name: $name)
+            }
+            """,
+            {},
+            {"data": {"bye": "Unknown"}},
+        ),
+        (
+            """
+            query ($name: String) {
+              bye(name: $name)
+            }
+            """,
+            {"name": "Name"},
+            {"data": {"bye": "Name"}},
+        ),
+        (
+            """
+            query ($name: String) {
+              bye(name: $name)
+            }
+            """,
+            {"name": None},
+            {
+                "data": {"bye": None},
+                "errors": [
+                    {
+                        "message": "Invalid value ("
+                        "value: None) for field `bye` of type `String`",
+                        "locations": [{"column": 15, "line": 3}],
+                        "path": ["bye"],
+                    }
+                ],
+            },
+        ),
+        (
+            """
+            query ($name: String!) {
+              bye(name: $name)
+            }
+            """,
+            {"name": None},
+            {
+                "data": None,
+                "errors": [
+                    {
+                        "message": "Given value for "
+                        "< name > is not "
+                        "type < <class "
+                        "'str'> >",
+                        "locations": [{"column": 20, "line": 2}],
+                        "path": None,
+                    }
+                ],
+            },
+        ),
+    ],
+)
+async def test_null_nullable(query, variables, expected):
+    assert await _TTFTT_ENGINE.execute(query, variables=variables) == expected
