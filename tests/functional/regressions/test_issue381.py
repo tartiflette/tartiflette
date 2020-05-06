5812aac0e4bccbe09f76ab834b9f00717841c930
Aurélien 'Bubu' Busi <aurelien.busi@dailymotion.com>
2020-05-06 11:32:19 +0200

    Add some tests cases

--- tests/functional/regressions/test_issue381.py
+++ tests/functional/regressions/test_issue381.py
@@ -2,67 +2,130 @@ import pytest
 
 from tartiflette import Directive, Resolver, TypeResolver, create_engine
 
-_SDL = """ schema {
-  query: Query
-}
 
-directive @run_me_this on OBJECT
+@pytest.fixture(scope="module")
+async def ttftt_engine_union():
+    schema_name = "test_issue381"
 
-type Query {
-  rootQueryField(resp: Int): CoupleOfResponses
-}
+    sdl = """ schema {
+        query: Query
+    }
 
-union CoupleOfResponses = ResponseOne | ResponseTwo
+    directive @run_me_this_object on OBJECT
+    directive @run_me_this_ifa on INTERFACE
+    directive @run_me_this_union on UNION
 
-type ResponseOne @run_me_this {
-    a: String
-}
+    type Query {
+        aFieldUnionObj: CoupleOfResponses
+        aFieldObj: CoupleOfResponsesNoUnionDir
+        aFieldIfa: AnIfa
+    }
 
+    union CoupleOfResponses @run_me_this_union = ResponseOne | ResponseTwo
+    union CoupleOfResponsesNoUnionDir = ResponseOne | ResponseTwo
 
-type ResponseTwo {
-   b: Int
-}
-"""
+    interface AnIfa @run_me_this_ifa {
+        a: String
+    }
 
+    type ResponseOne @run_me_this_object {
+        a: String
+    }
 
-@Directive(name="run_me_this", schema_name="test_issue381")
-class RunMeThis:
-    async def on_pre_output_coercion(
-        self, directive_args, next_directive, value, ctx, info
-    ):
-        bob = await next_directive(value, ctx, info)
-        return {"a": f"{bob['a']} 2"}
+    type ResponseThree implements AnIfa {
+        a: String
+    }
 
+    type ResponseTwo {
+        b: Int
+    }
+    """
 
-@Resolver("Query.rootQueryField", schema_name="test_issue381")
-async def resolve_root_query_field(pr, args, ctx, info):
-    return {"a": "LOOOL "}
+    @Directive(name="run_me_this_object", schema_name=schema_name)
+    class RunMeThisObject:
+        async def on_pre_output_coercion(
+            self, directive_args, next_directive, value, ctx, info
+        ):
+            bob = await next_directive(value, ctx, info)
+            return {"a": f"{bob['a']} obj"}
 
+    @Directive(name="run_me_this_union", schema_name=schema_name)
+    class RunMeThisUnion:
+        async def on_pre_output_coercion(
+            self, directive_args, next_directive, value, ctx, info
+        ):
+            bob = await next_directive(value, ctx, info)
+            return {"a": f"{bob['a']} union"}
 
-@TypeResolver("CoupleOfResponses", schema_name="test_issue381")
-def resolve_type_couple_of_responses(result, context, info, abstract_type):
-    if "a" in result:
-        return "ResponseOne"
-    return "ResponseTwo"
+    @Directive(name="run_me_this_ifa", schema_name=schema_name)
+    class RunMeThisIfa:
+        async def on_pre_output_coercion(
+            self, directive_args, next_directive, value, ctx, info
+        ):
+            bob = await next_directive(value, ctx, info)
+            return {"a": f"{bob['a']} ifa"}
+
+    @Resolver("Query.aFieldUnionObj", schema_name=schema_name)
+    @Resolver("Query.aFieldObj", schema_name=schema_name)
+    @Resolver("Query.aFieldIfa", schema_name=schema_name)
+    async def resolve_root_query_field(pr, args, ctx, info):
+        return {"a": "LOOOL"}
+
+    @TypeResolver("CoupleOfResponses", schema_name=schema_name)
+    @TypeResolver("CoupleOfResponsesNoUnionDir", schema_name=schema_name)
+    def resolve_type_couple_of_responses(result, context, info, abstract_type):
+        if "a" in result:
+            return "ResponseOne"
+        return "ResponseTwo"
+
+    @TypeResolver("AnIfa", schema_name=schema_name)
+    def resolve_type_ifa(result, context, info, abstract_type):
+        return "ResponseThree"
+
+    return await create_engine(sdl=sdl, schema_name=schema_name)
 
 
 @pytest.mark.asyncio
-async def test_issue381_output_coercion_on_union():
-    e = await create_engine(sdl=_SDL, schema_name="test_issue381")
-    assert (
-        await e.execute(
+@pytest.mark.parametrize(
+    "query,expected",
+    [
+        (
             """
 query {
-    rootQueryField(resp: 1) {
+    aFieldUnionObj {
         ...on ResponseOne {
             a
         }
-        ...on ResponseTwo {
-            b
+    }
+}
+""",
+            {"data": {"aFieldUnionObj": {"a": "LOOOL union obj"}}},
+        ),
+        (
+            """
+query {
+    aFieldObj {
+        ...on ResponseOne {
+            a
         }
     }
 }
-"""
-        )
-        == {"data": {"rootQueryField": {"a": "LOOOL  2"}}}
-    )
+            """,
+            {"data": {"aFieldObj": {"a": "LOOOL obj"}}},
+        ),
+        (
+            """
+query {
+    aFieldIfa {
+        ...on ResponseThree { a }
+    }
+}
+            """,
+            {"data": {"aFieldIfa": {"a": "LOOOL ifa"}}},
+        ),
+    ],
+)
+async def test_issue381_output_coercion_on_union(
+    ttftt_engine_union, query, expected
+):
+    assert await ttftt_engine_union.execute(query) == expected
