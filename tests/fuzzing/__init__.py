2307035e6ed2b69696e338f36a0ffdfc8f3989ed
Maximilien Raulic <raulic.maximilien@gmail.com>
2020-08-10 21:11:39 +0200

    test(fuzzing): remove outdated tests

--- tests/fuzzing/__init__.py
+++
@@ -1,7 +0,0 @@
-# To run the fuzzing test:
-# py-afl-fuzz -o results-path/ -i /path/to/sdl_examples --  /path/to/env/bin/python /path/to/the/test_build_graphql_schema_from_sdl_fuzz.py
-
-# Installation requirements:
-# brew install afl-fuzz
-# pip install python-afl
-# See https://alexgaynor.net/2015/apr/13/introduction-to-fuzzing-in-python-with-afl/
