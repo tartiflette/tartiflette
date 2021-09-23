779d54297b31ea2bd00ee13779dbfe09d368d322
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2021-09-23 22:46:04 +0200

    chore(setup): set metadata in setup.cfg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import sys
 
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 from setuptools.command.build_py import build_py
 
@@ -51,55 +51,7 @@ class LibGraphQLParserExtension(Extension):
         super().__init__("libgraphqlparser", sources=[])
 
 
-_TEST_REQUIRE = [
-    "pytest==6.2.5",
-    "pytest-cov==2.12.1",
-    "pytest-asyncio==0.15.1",
-    "pytest-xdist==2.3.0",
-    "pylint==2.10.2",
-    "black==21.8b0",
-    "isort==5.9.3",
-]
-
-_BENCHMARK_REQUIRE = ["pytest-benchmark==3.4.1"]
-
-_VERSION = "1.4.0"
-
-_PACKAGES = find_packages(exclude=["tests*"])
-
-
-def _read_file(filename):
-    with open(filename) as afile:
-        return afile.read()
-
-
 setup(
-    name="tartiflette",
-    version=_VERSION,
-    description="GraphQL Engine for Python",
-    long_description=_read_file("README.md"),
-    long_description_content_type="text/markdown",
-    url="https://github.com/tartiflette/tartiflette",
-    author="Dailymotion Core API Team",
-    author_email="team@tartiflette.io",
-    license="MIT",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-    keywords="api graphql protocol api rest relay tartiflette dailymotion",
-    packages=_PACKAGES,
-    install_requires=["cffi>=1.0.0,<2.0.0", "lark-parser==0.12.0"],
-    tests_require=_TEST_REQUIRE,
-    extras_require={"test": _TEST_REQUIRE, "benchmark": _BENCHMARK_REQUIRE},
     cmdclass={"build_ext": BuildExtCmd, "build_py": BuildPyCmd},
     ext_modules=[LibGraphQLParserExtension()],
-    include_package_data=True,
 )
